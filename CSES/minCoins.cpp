4e1285763de1add696e6cfb105f2ecbfef9d6694
rajtripathi08@outlook.com <rajtripathi08@outlook.com>
2026-07-27 16:05:41 +0530

    memoizedCode

--- CSES/minCoins.cpp
+++ CSES/minCoins.cpp
@@ -1,46 +1,46 @@
 #include<bits/stdc++.h>
 using namespace std;
-// int dp[1000001][101];
-// int solve(vector<int> &c, int idx, int n)
-// {
-//     if(idx >= c.size() || n <= 0)
-//         return (n == 0)? 0 : INT_MAX - 1;
+int dp[1000001][101];
+int solve(vector<int> &c, int idx, int n)
+{
+    if(idx >= c.size() || n <= 0)
+        return (n == 0)? 0 : INT_MAX - 1;
 
-//     int res = -1;
+    int res = -1;
 
-//     if(dp[n][idx] != -1)    
-//         return dp[n][idx];
+    if(dp[n][idx] != -1)    
+        return dp[n][idx];
 
-//     if(c[idx] > n)
-//     {
-//         int notPick = solve(c, idx+1, n);
-//         dp[n][idx] = res = notPick;
-//     }
-//     else
-//     {
-//         int pick = 1 + solve(c, idx, n - c[idx]);
-//         int notPick = solve(c, idx+1, n);
-//         dp[n][idx] = res = min(pick, notPick);
-//     }
+    if(c[idx] > n)
+    {
+        int notPick = solve(c, idx+1, n);
+        dp[n][idx] = res = notPick;
+    }
+    else
+    {
+        int pick = 1 + solve(c, idx, n - c[idx]);
+        int notPick = solve(c, idx+1, n);
+        dp[n][idx] = res = min(pick, notPick);
+    }
 
-//     return dp[n][idx] = res;
-// }
+    return dp[n][idx] = res;
+}
 
-// int main()
-// {
-//     int n;
-//     cin >> n;
+int main()
+{
+    int n;
+    cin >> n;
 
-//     int x;
-//     cin >> x;
-//     vector<int> c(n);
-//     for(int &a : c)
-//         cin >> a;
-//     memset(dp, -1, sizeof(dp));
-//     int res = solve(c, 0, x);
-//     res = (res == INT_MAX-1)? -1 : res;
-//     cout << res;
-// }
+    int x;
+    cin >> x;
+    vector<int> c(n);
+    for(int &a : c)
+        cin >> a;
+    memset(dp, -1, sizeof(dp));
+    int res = solve(c, 0, x);
+    res = (res == INT_MAX-1)? -1 : res;
+    cout << res;
+}
 
 int main() {
     int n, x;
